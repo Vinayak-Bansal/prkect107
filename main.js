8e9d7e3154f7fb9467c70204b470a1dd3997890c
Vinayak-Bansal <97925994+Vinayak-Bansal@users.noreply.github.com>
2023-07-24 10:46:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true})
+    classifier= ml5.soundClassifier()('https://teachablemachine.withgoogle.com/models/Ka4f-80yW/model.json', modelReady)
+}
+function modelReady() {
+    classifier.classify(gotResults)
+}
+function gotResults(error, results){
+console.log('Got Result')
+}
+
+function Identify () {
+
+}
